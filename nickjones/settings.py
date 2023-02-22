e4f9d281176d9dcccd236f0d741195efe3720569
Kazuo Kaze <kazuokaze00@gmail.com>
2023-02-22 21:03:12 +0530

    Ninth Deploying to Railway

--- nickjones/settings.py
+++ nickjones/settings.py
@@ -39,7 +39,7 @@ DEBUG = True
 if not DEBUG:
     CSRF_TRUSTED_ORIGINS = ['https://surround-yorself-with-killers-production.up.railway.app/']
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://surround-yorself-with-killers-production.up.railway.app']
 
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'frontpage'
